bf1987b1db72592762c5488595e3a9e33ffd8825
Dhruv Parikh <dhruv.parikh@ecsg.com.au>
2020-10-01 10:28:39 +0530

    #1 Added sample code for ts express

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nodets-boilerplate
 NodeTS Boilerplate Code
+
+Installation
+
+    npm install
+    npm start
+
+It includes ts dependency which will build dist folder on prestart / build command.
+This project will give you a kickstart for building express app using typescripts.
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.json( { data: [] } );
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
